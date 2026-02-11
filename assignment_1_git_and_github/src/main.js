f417af3fbeca4e1466f865b012197249daad7703
Max <max.yang342@gmail.com>
2026-02-11 11:40:07 -0500

    create animation loop and torus

--- assignment_1_git_and_github/src/main.js
+++ assignment_1_git_and_github/src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three"
 
+// Initialize three js scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -17,3 +18,15 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({colour: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Animation loop
+function animate() {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
